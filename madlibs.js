db3a75730f3b472838f2347e4dc8241626c44598
k-857 <60834543+k-857@users.noreply.github.com>
2022-02-16 20:45:10 -0500

    Add files via upload

---
+++ madlibs.js
@@ -0,0 +1,31 @@
+
+  
+function upper(el) {  
+    var str = el.value;  
+    el.value = str.toUpperCase();
+}
+
+function createMessage() {
+    document.getElementById("result").style.display = "inline";
+    document.getElementById("word1").innerHTML=
+        document.getElementById("pluralNoun").value;
+    document.getElementById("word2").innerHTML=
+        document.getElementById("noun1").value;
+    document.getElementById("word3").innerHTML=
+        document.getElementById("noun2").value;
+    document.getElementById("word4").innerHTML=
+        document.getElementById("adj").value;
+    document.getElementById("word5").innerHTML=
+        document.getElementById("verb").value;
+ 
+    upper(document.getElementById("pluralNoun")); 
+    upper(document.getElementById("noun1")); 
+    upper(document.getElementById("noun2")); 
+    upper(document.getElementById("adj")); 
+    upper(document.getElementById("verb")); 
+}
+
+function goAgain() {
+    location.reload(); 
+    return false; 
+}
